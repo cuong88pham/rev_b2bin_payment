c3da9a6c4e809c78ae02c368e0427d8606d59a0c
Cuongpm <harry88pham@gmail.com>
2018-12-06 14:15:28 +0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RevB2binPayment::Engine.routes.draw do
+	namespace :wallet do
 		get 'deposit1' => 'b2bin_payment_deposit#index'
 		get 'deposit/:code' => 'b2bin_payment_deposit#create', as: 'create_b2bin_payment'
     get 'deposit/:code/address' => 'b2bin_payment_deposit#show', as: 'view_b2bin_payment'
+  end
 end
